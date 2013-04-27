7d386e1fe308c74a4c1b38e1bbe2f1890ff2173f
Masahiro Nakagawa <repeatedly@gmail.com>
2013-04-27 01:43:34 -0700

    Fix prefix usage

--- app.rb
+++ app.rb
@@ -247,7 +247,7 @@ helpers do
   end
 
   def prefix
-    (@current_lang.nil? || @current_lang == $DEFAULT_LANGUAGE) ? '' : "/#{@current_lang}"
+    (@current_lang.nil? || @current_lang == $DEFAULT_LANGUAGE) ? '' : "#{@current_lang}/"
   end
 
   def avaiable_language?(article, lang)
