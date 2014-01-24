8adb5fa1efb12f8a8beb90513604086da087cb9b
Masahiro Nakagawa <repeatedly@gmail.com>
2014-01-24 18:41:37 +0900

    Render ### sub-section in 'Table of Contents'

--- lib/article.rb
+++ lib/article.rb
@@ -2,6 +2,7 @@ Encoding.default_external = Encoding.default_internal = 'utf-8'
 
 require 'rdiscount'
 require 'sanitize'
+require 'strscan'
 
 class Article
   def text_only
@@ -85,7 +86,44 @@ class Article
     title.downcase.gsub(/[^a-z0-9 -]/, '').gsub(/ /, '-')
   end
 
+  H2_RE = /<h2>([^<]+)<\/h2>/m
+  H3_RE = /<h3>([^<]+)<\/h3>/m
+
   def _toc(content)
+    toc = _scan_toc(content)
+    content = content.gsub(H2_RE) { |m|
+      "<a name=\"#{slugify(m.gsub(/<[^>]+>/, ''))}\"></a>#{m}"
+    }
+    content = content.gsub(H3_RE) { |m|
+      "<a name=\"#{slugify(m.gsub(/<[^>]+>/, ''))}\"></a>#{m}"
+    }
+    return toc, content
+  end
+
+  def _scan_toc(content)
+    ss = StringScanner.new(content)
+    tocs = {}
+    post_match = nil
+    while matched = ss.scan_until(H2_RE)
+      section = ss.matched.scan(H2_RE).first.first
+      cur_pos = ss.pos
+      post_match = ss.post_match
+      ss.check_until(H2_RE)
+      tocs[section] = _scan_sub_toc(ss.pre_match ? ss.pre_match[cur_pos..-1] : post_match)
+    end
+    tocs
+  end
+
+  def _scan_sub_toc(content)
+    ss = StringScanner.new(content)
+    toc = []
+    while matched = ss.scan_until(H3_RE)
+      toc << ss.matched.scan(H3_RE).first.first
+    end
+    toc
+  end
+
+  def _toc_(content)
     toc = content.scan(/<h2>([^<]+)<\/h2>/m).to_a.map { |m| m.first }
     content_with_anchors = content.gsub(/(<h2>[^<]+<\/h2>)/m) do |m|
       "<a name=\"#{slugify(m.gsub(/<[^>]+>/, ''))}\"></a>#{m}"
