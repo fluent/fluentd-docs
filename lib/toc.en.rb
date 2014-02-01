029c7602913fe973a311b5f3fe0f36b4bd1b5aed
Kazuki Ohta <kazuki.ohta@gmail.com>
2014-02-01 14:18:52 -0800

    add community page

--- lib/toc.en.rb
+++ lib/toc.en.rb
@@ -209,6 +209,9 @@ section 'developer', 'Developer' do
   category 'plugin-development', 'Plugin Development' do
     article 'plugin-development', 'Plugin Development'
   end
+  category 'community', 'Community' do
+    article 'community', 'Community'
+  end
   category 'mailing-list', 'Mailing List' do
     article 'mailing-list', 'Mailing List'
   end
