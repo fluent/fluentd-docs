71d56ff53cc9fa709d4a6c63c137afac6153fbb1
Jun Hiroe <Jun.Hiroe@gmail.com>
2014-05-27 11:03:39 +0900

    Add out_secure_forward menu in Japanese

--- lib/toc.ja.rb
+++ lib/toc.ja.rb
@@ -154,6 +154,9 @@ section 'output-plugins', 'Output Plugins' do
   category 'out_forward', 'out_forward' do
     article 'out_forward', 'forward Output Plugin'
   end
+  category 'out_secure_forward', 'out_secure_forward' do
+    article 'out_secure_forward', 'secure_forward Output Plugin', ['SSL', 'authentication']
+  end
   category 'out_exec', 'out_exec' do
     article 'out_exec', 'exec Output Plugin'
   end
