d958886f2880965a81eb78035eb0bacd96682494
Kiyoto Tamura <me@ktamura.com>
2014-12-14 23:52:57 +0000

    update the v0.12 tests and remove the offensive blank file

--- test/test_basic.rb
+++ test/test_basic.rb
@@ -9,6 +9,8 @@ class FluentdDocsTest < MiniTest::Unit::TestCase
   end
 
   docs_path = File.expand_path('../../docs', __FILE__)
+  
+  # generate tests for the current stable versions and i18n for them
   Dir.glob("#{docs_path}/*.txt") do |url_part|
     url_part.gsub!(/^#{docs_path}\//, "")
     # Skip macro files, which start with "_"
@@ -23,10 +25,20 @@ class FluentdDocsTest < MiniTest::Unit::TestCase
         get "/articles/#{url_part.split("/", 2).last}"
         assert last_response.ok?
       end
+    end
+  end
+
+  # generate tests for the new versions
+  Dir.glob("#{docs_path}/v0.12/*.txt") do |url_part|
+    url_part.gsub!(/^#{docs_path}\/v0.12\//, "")
+    # Skip macro files, which start with "_"
+    if not url_part.split("/").last.start_with?("_")
+      url_part.gsub!(/.txt$/, '')
+      method_name = url_part.gsub(/[-\/]/, '_')
       define_method("test_v0_12_#{method_name}".to_s) do
         get "/v0.12/articles/#{url_part.split("/", 2).last}"
         assert last_response.ok?
       end
-    end
+    end    
   end
 end
