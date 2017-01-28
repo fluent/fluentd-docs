b1355d4ef74090aa05001481fe3208d663ac1c40
Kazuki Ohta <kazuki.ohta@gmail.com>
2017-01-27 23:34:32 -0800

    out_kafka: added

--- lib/toc.en.v0.12.rb
+++ lib/toc.en.v0.12.rb
@@ -198,6 +198,9 @@ section 'output-plugins', 'Output Plugins' do
   category 'out_s3', 'out_s3' do
     article 'out_s3', 'S3 Output Plugin', ['Amazon S3', 'AWS', 'Simple Storage Service']
   end
+  category 'out_kafka', 'out_kafka' do
+    article 'out_kafka', 'Kafka Output Plugin'
+  end
   category 'out_mongo', 'out_mongo' do
     article 'out_mongo', 'MongoDB Output Plugin', ['MongoDB']
   end
